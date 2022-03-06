3e038ac8d56b2760db8faa8a3144b48ef2b75f8b
mayanksparsh2 <40720647+mayanksparsh2@users.noreply.github.com>
2022-03-06 16:35:45 +0530

    Add files via upload

---
+++ src/Mar03HandsOn/src/main/java/com/org/demo/AdvancewebservicesAssignment3rdMarchApplication.java
@@ -0,0 +1,13 @@
+package com.org.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancewebservicesAssignment3rdMarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancewebservicesAssignment3rdMarchApplication.class, args);
+	}
+
+}
---
+++ src/Mar03HandsOn/src/test/java/com/org/demo/AdvancewebservicesAssignment3rdMarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancewebservicesAssignment3rdMarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
